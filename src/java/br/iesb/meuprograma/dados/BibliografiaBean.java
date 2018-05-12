cb9c8e0134af37dd5e193705c2c1b2f817d0088b
Juliana Costa <31804288+bg21@users.noreply.github.com>
2018-05-12 19:52:13 -0300

    Atualizando BibliografiaBEAN/DAO

---
+++ src/java/br/iesb/meuprograma/dados/BibliografiaBean.java
@@ -0,0 +1,105 @@
+package br.iesb.meuprograma.dados;
+import br.iesb.meuprograma.negocio.BibliografiaBO;
+import br.iesb.meuprograma.negocio.NegocioException;
+import java.io.Serializable;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean(name = "BibliografiaBean")
+@SessionScoped
+public class BibliografiaBean implements Serializable{
+    private int id;
+    private String cursos;
+    private String disciplinas;
+    private String titulo;
+    private String autor;
+    private int ano;
+    private String isbn;
+    private String editora;  
+    
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCursos() {
+        return cursos;
+    }
+
+    public void setCursos(String cursos) {
+        this.cursos = cursos;
+    }
+
+    public String getDisciplinas() {
+        return disciplinas;
+    }
+
+    public void setDisciplinas(String disciplinas) {
+        this.disciplinas = disciplinas;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+    
+    @Override
+    public String toString(){
+        return titulo;
+    }
+    
+    public void cadastrarBibliografia() {
+        BibliografiaBO bo = new BibliografiaBO(); 
+        
+        try{
+            bo.inserir(this);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Sucesso", "Dados inseridos com sucesso!")); 
+        }catch(NegocioException ex){
+            if(ex.getCause() != null){
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Erro", ex.getMessage()));
+            }else{
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Erro", ex.getMessage())); 
+            }
+        }
+    }
+}
---
+++ src/java/br/iesb/meuprograma/dados/BibliografiaDAO.java
@@ -0,0 +1,129 @@
+package br.iesb.meuprograma.dados;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BibliografiaDAO implements DAO<BibliografiaBean> {
+
+    @Override
+    public void inserir(BibliografiaBean entidade) throws DadosException {
+        Connection conexao = ConexaoBD.getConexao();
+        String sql = "INSERT INTO tb_bibliografia (cursos, disciplinas, titulo, autor, ano, isbn, editora) VALUES (?, ?, ?, ?, ?, ?, ?)";
+       
+        try {
+            PreparedStatement comando = conexao.prepareStatement(sql);
+            comando.setString(1, entidade.getCursos());
+            comando.setString(2, entidade.getDisciplinas());
+            comando.setString(3, entidade.getTitulo());
+            comando.setString(4, entidade.getAutor());
+            comando.setInt(5, entidade.getAno());
+            comando.setString(6, entidade.getIsbn());
+            comando.setString(7, entidade.getEditora());
+            comando.executeUpdate();
+            conexao.close();
+        } catch (SQLException ex) {
+            throw new DadosException("Erro ao inserir dados!", ex);
+        }       
+    }	
+
+    @Override
+    public void alterar(BibliografiaBean entidade) throws DadosException {
+        Connection conexao = ConexaoBD.getConexao();
+        String sql = "UPDATE tb_bibliografia SET cursos=?, disciplinas=?, titulo=?, autor=?, ano=?, isbn=?, editora=? WHERE id_bibliografia=?";
+        try {
+            PreparedStatement comando = conexao.prepareStatement(sql);
+            comando.setString(1, entidade.getCursos());
+            comando.setString(2, entidade.getDisciplinas());
+            comando.setString(3, entidade.getTitulo());
+            comando.setString(4, entidade.getAutor());
+            comando.setInt(5, entidade.getAno());
+            comando.setString(6, entidade.getIsbn());
+            comando.setString(7, entidade.getEditora());
+            comando.setInt(8, entidade.getId());
+            comando.executeUpdate();
+            conexao.close();
+        } catch (SQLException ex) {
+            throw new DadosException("Erro ao alterar!\n" + ex.getMessage(), ex);
+        }
+    }
+
+    @Override
+    public void excluir(BibliografiaBean entidade) throws DadosException {
+        Connection conexao = ConexaoBD.getConexao();
+        String sql = "DELETE FROM tb_bibliografia WHERE id_bibliografia=?";
+        
+        try {
+            PreparedStatement comando = conexao.prepareStatement(sql);
+            comando.setInt(1, entidade.getId());
+            comando.executeUpdate();
+            conexao.close();
+        } catch (SQLException ex) {
+            throw new DadosException("Erro ao excluir!\n" + ex.getMessage(), ex);
+        }
+    }
+
+    @Override
+    public BibliografiaBean consultar(int id) throws DadosException {
+        Connection conexao = ConexaoBD.getConexao();
+        String sql = "SELECT * FROM tb_bibliografia WHERE id_bibliografia=?";
+        BibliografiaBean cadBiblio = new BibliografiaBean();
+        
+        try {
+            PreparedStatement comando = conexao.prepareStatement(sql);
+            comando.setInt(1, id);
+            ResultSet resultado = comando.executeQuery();
+            
+            if(resultado.next()){
+                cadBiblio.setId(resultado.getInt(1));
+                cadBiblio.setCursos(resultado.getString(2));
+                cadBiblio.setDisciplinas(resultado.getString(3));
+                cadBiblio.setTitulo(resultado.getString(4));
+                cadBiblio.setAutor(resultado.getString(5));
+                cadBiblio.setAno(resultado.getInt(6));
+                cadBiblio.setIsbn(resultado.getString(7));
+                cadBiblio.setEditora(resultado.getString(8));
+            
+            }
+            conexao.close();
+        } catch (SQLException ex) {
+            throw new DadosException("Erro ao consultar!\n" + ex.getMessage(), ex);
+        }
+        return cadBiblio;
+    }
+
+    @Override
+    public List<BibliografiaBean> listar() throws DadosException {
+        Connection conexao = ConexaoBD.getConexao();
+        String sql = "SELECT * FROM tb_bibliografia";
+        List <BibliografiaBean> lista = new ArrayList<>();
+                
+        try {
+            Statement comando = conexao.createStatement();
+            ResultSet resultado = comando.executeQuery(sql);
+            
+            while (resultado.next()){
+                BibliografiaBean cadBiblio = new BibliografiaBean();
+                cadBiblio.setId(resultado.getInt(1));
+                cadBiblio.setCursos(resultado.getString(2));
+                cadBiblio.setDisciplinas(resultado.getString(3));
+                cadBiblio.setTitulo(resultado.getString(4));
+                cadBiblio.setAutor(resultado.getString(5));
+                cadBiblio.setAno(resultado.getInt(6));
+                cadBiblio.setIsbn(resultado.getString(7));
+                cadBiblio.setEditora(resultado.getString(8));
+                lista.add(cadBiblio);
+            }
+            conexao.close();
+        } catch (SQLException ex) {
+            throw new DadosException("Erro ao listar!\n" + ex.getMessage(), ex);
+        }
+        return lista;
+    }
+
+    
+    
+}
