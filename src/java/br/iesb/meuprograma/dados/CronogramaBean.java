e270bd80ee71ae755934abe73b2544acb4814bcb
Juliana Costa <31804288+bg21@users.noreply.github.com>
2018-05-12 22:34:02 -0300

    Update and rename Cronograma.java to CronogramaBean.java

---
+++ src/java/br/iesb/meuprograma/dados/CronogramaBean.java
@@ -0,0 +1,62 @@
+package br.iesb.meuprograma.dados;
+
+import br.iesb.meuprograma.negocio.CronogramaBO;
+import br.iesb.meuprograma.negocio.NegocioException;
+import java.io.Serializable;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean(name = "CronogramaBean")
+@SessionScoped
+public class CronogramaBean implements Serializable{
+    private int id;
+    private int aula;
+    private String conteudo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAula() {
+        return aula;
+    }
+
+    public void setAula(int aula) {
+        this.aula = aula;
+    }
+
+    public String getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
+    
+    @Override
+    public String toString(){
+        return "Aula: " + aula + "  Conteudo: " + conteudo;
+    }
+    
+    public void cadastrarCronograma() {
+        CronogramaBO bo = new CronogramaBO(); 
+        
+        try{
+            bo.inserir(this);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Sucesso", "Dados inseridos com sucesso!")); 
+        }catch(NegocioException ex){
+            if(ex.getCause() != null){
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Erro", ex.getMessage()));
+            }else{
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Erro", ex.getMessage())); 
+            }
+        }
+    }
+    
+}
